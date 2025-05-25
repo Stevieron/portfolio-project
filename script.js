b6e98f6f84159c8915a55e7c7a1565aab9e5569c
Stephen Okon <stevenbassey07@gmail.com>
2025-05-25 22:41:52 +0100

    feat: add menu icon and JavaScript logic for menu toggle

---
+++ script.js
@@ -0,0 +1,7 @@
+const navLinks = document.querySelector(".nav-links");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  hamburger.classList.toggle("open");
+});
