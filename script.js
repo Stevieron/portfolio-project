7a51592d7c83cd7e0c3ce3e8765721c8502ba154
Stephen Okon <stevenbassey07@gmail.com>
2025-05-26 12:12:13 +0100

    feat: implement design feature and smooth scroll to correct page view of nav links

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
   hamburger.classList.toggle("open");
 });
+
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = link.getAttribute("href");
+    const target = document.querySelector(targetId);
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+
+    navLinks.classList.remove("open");
+    hamburger.classList.remove("open");
+  });
+});
