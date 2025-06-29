1f1f7e41d1852d5a8f3cbaefab41f76cecf5fd10
Stephen Okon <stevenbassey07@gmail.com>
2025-06-29 22:52:21 +0100

    feat: add media files and new project cards

--- script.js
+++ script.js
@@ -24,74 +24,74 @@ document.querySelectorAll('a[href^="#"]').forEach((link) => {
 const projects = [
   {
     title: "JennyPet Online Store",
-    subtitle: "Adopt adorable pets at a good price",
+    subtitle: "Adopt a furry friend today",
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups  required. Has been the industry's standard.",
+      "Discover your perfect pet match with Jenny Store—adopt cats, dogs, and rabbits easily and safely online.",
     fullDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+      "Jenny Pet Online Store is a dynamic and user-friendly adoption platform that connects loving homes with healthy, vaccinated pets. Designed with responsive layouts, a fun pricing section, and engaging JavaScript features like hero animations and hamburger navigation, the site is a perfect example of practical frontend interactivity. Pets come with starter kits, star ratings, and users can explore various pet types. The UI design is inspired by real-world Figma prototypes and emphasizes accessibility and seamless experience across devices. Pets come with starter kits, star ratings, and users can explore various pet types. The UI design is inspired by real-world Figma prototypes and emphasizes accessibility and seamless experience across devices.",
 
     tech: ["HTML", "CSS", "JavaScript"],
-    featuredImage: "/media-files/Detail-Image.png",
+    featuredImage: "/media-files/Jenny-Img-Store.jpeg",
     live: "https://www.live-link.com",
-    repo: "https://github.com/Stevieron",
+    repo: "https://github.com/Stevieron/jenny-pet-store",
   },
   {
     title: "Typicode-Todos",
-    subtitle: "Priority is the fuel of time management",
+    subtitle: "Priorities first",
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups  required. Has been the industry's standard.",
+      "A responsive To-Do app powered by Typicode API with CRUD operations and sleek UI interactions.",
     fullDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+      "Typicode-Todos is a task management web application built with clean JavaScript and connected to the JSONPlaceholder API. Users can add, update, mark complete, and delete tasks in real time. The app utilizes modular JavaScript, custom styling, and mobile-first responsiveness. With persistent state management via API calls, intuitive feedback messages, and visual alerts, Typicode-Todos simulates production-level interactivity while demonstrating key concepts in asynchronous JS and UI logic design.",
     tech: ["HTML", "Tailwind", "React"],
     featuredImage: "/media-files/Detail-Image.png",
     live: "https://www.live-link.com",
-    repo: "https://github.com/Stevieron",
+    repo: "https://github.com/Stevieron/typicode-todos",
   },
   {
     title: "Joke-Generator",
     subtitle: "Laugh out the stress with our little joke app",
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups  required. Has been the industry's standard.",
+      "A user-friendly application that instantly generate hilarious jokes with a click—powered by JokeAPI for endless fun.",
     fullDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+      "The Joke Generator App fetches real-time jokes using an external API and displays them interactively. The UI features custom loaders, a typing effect during fetch, and styled quote boxes. With added features like dark mode, share-to-social options, and a history log, the app encourages user engagement. The project showcases event-driven JS, clean code organization, and a polished minimalist aesthetic. It also helps beginners understand DOM manipulation and working with third-party data.",
     tech: ["HTML", "Tailwind", "React"],
     featuredImage: "/media-files/Detail-Image.png",
     live: "https://www.live-link.com",
-    repo: "https://github.com/Stevieron",
+    repo: "https://github.com/Stevieron/joke-generator",
   },
   {
     title: "Flixx App",
     subtitle: "Watch any movie of your choice on Flixx",
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups  required. Has been the industry's standard.",
+      "A modern movie web app displaying trending films, genres, and search using The Movie DB API.",
     fullDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+      "Flixx App is a fully responsive movie discovery platform featuring up-to-date data from TMDB. Users can explore trending movies, genre-specific filters, and detailed film information via modals. Built with semantic HTML, grid-based layouts, and optimized JS, Flixx also includes features like theme toggling, search auto-suggestions, and favorites list via localStorage. It is a demonstration of frontend mastery in handling large datasets, API responses, and dynamic user interface creation.",
     tech: ["HTML", "Tailwind", "React"],
-    featuredImage: "/media-files/Detail-Image.png",
+    featuredImage: "/media-files/showcase-bg.jpg",
     live: "https://www.live-link.com",
-    repo: "https://github.com/Stevieron",
+    repo: "https://github.com/Stevieron/flixx-app",
   },
   {
-    title: "GreenTech",
-    subtitle: "Protect our ecosystem via GreenTech solutions",
+    title: "Smart Transport",
+    subtitle: "Data privacy is key!",
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups  required. Has been the industry's standard.",
+      "A online law repository for global smart transportation policies—scraping and displaying legal data across countries such as GDPR. ",
     fullDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+      "Smart Transport is a modern, data-driven web app designed to serve as a centralized legal repository for smart transportation laws worldwide. The app scrapes, organizes, and displays legislation on autonomous vehicles, electric mobility, ride-sharing, and public transport innovation. Built with a focus on usability and structured content, it includes search and filter functionalities, country-based categorization, and modal views for in-depth law descriptions. The project reflects real-world use of data aggregation, information architecture, and frontend rendering of complex datasets, offering a scalable and user-centered solution for legal tech applications.",
     tech: ["HTML", "CSS", "JavaScript"],
-    featuredImage: "/media-files/Detail-Image.png",
+    featuredImage: "/media-files/smart_transport.jpg",
     live: "https://www.live-link.com",
-    repo: "https://github.com/Stevieron",
+    repo: "https://github.com/Stevieron/smart_transport",
   },
   {
     title: "CodeRite",
     subtitle: "Learn software development at CodeRite",
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups  required. Has been the industry's standard.",
+      "Learn software development the right way—CodeRite offers guided learning paths for aspiring developers.",
     fullDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. \nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      "CodeRite is a simulated learning platform focused on teaching software development skills through curated modules, interactive challenges, and mentorship features. The site includes lesson highlights, tech stack breakdowns, progress tracking indicators, and a call-to-action for joining coding cohorts. While static, the design represents scalable education platforms with real-world structure. This project demonstrates mastery of layout control, component styling, and front-facing course content architecture. CodeRite also serves as an online platform and a community of like-minded and passionate engineers, willing to birth realistic ideas and innovative digital solutions. ",
     tech: ["HTML", "CSS", "JavaScript"],
-    featuredImage: "/media-files/Detail-Image.png",
+    featuredImage: "/media-files/coderite_image.jpeg",
     live: "https://www.live-link.com",
     repo: "https://github.com/Stevieron",
   },
